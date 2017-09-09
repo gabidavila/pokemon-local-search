f1c060df7ea7b7767a3096bf17c33160be407a3f
gabidavila <gabidavila@gmail.com>
2017-09-09 16:55:25 -0400

    added pagination

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
   const pokemonContainer = document.getElementById("pokemon-container");
   const allPokemonsList = search();
   const paginatedPokemon = paginate(allPokemonsList, offset);
+  const nextButton = document.getElementById("next");
+  const previousButton = document.getElementById("previous");
+
   render(paginatedPokemon, pokemonContainer);
 
   pokemonContainer.addEventListener("click", function (event) {
@@ -11,5 +14,28 @@ document.addEventListener("DOMContentLoaded", function() {
       flip(event.target.parentElement);
     }
   });
-  // console.log(paginatedPokemon);
+
+  nextButton.addEventListener("click", function (event) {
+    if(offset + limit > allPokemonsList.length) {
+      return;
+    }
+
+    pokemonContainer.innerHTML = "";
+    offset += limit;
+    
+    const paginatedPokemon = paginate(allPokemonsList, offset, offset + limit);
+    render(paginatedPokemon, pokemonContainer);
+  });
+
+  previous.addEventListener("click", function (event) {
+    if(offset - limit < 0) {
+      return;
+    }
+    const previousOffset = offset;
+    pokemonContainer.innerHTML = "";
+    offset -= limit;
+
+    const paginatedPokemon = paginate(allPokemonsList, offset, previousOffset);
+    render(paginatedPokemon, pokemonContainer);
+  });
 });
